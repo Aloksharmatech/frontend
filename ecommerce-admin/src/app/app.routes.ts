377fc48bbfc9840760698262d32cc4636418d58a
aloksharmatech <sharma.alok.tech@gmail.com>
2025-07-14 20:15:52 +0530

    inital

---
+++ ecommerce-admin/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+// src/app/app.routes.ts
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./product/product-module').then((m) => m.default),
+  },
+];
---
+++ ecommerce-admin/src/app/product/product-module.ts
@@ -0,0 +1,3 @@
+// product/product-module.ts
+import { productRoutes } from './product.routes';
+export default productRoutes;
